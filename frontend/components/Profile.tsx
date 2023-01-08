90a8d9c328c952097f2df2cde9bea3917f436215
Noah Lu <luzhenqian98@gmail.com>
2023-01-08 16:04:53 +0800

    feat: add network switch

--- frontend/components/Profile.tsx
+++ frontend/components/Profile.tsx
@@ -6,25 +6,65 @@ import {
   useEnsAvatar,
   useEnsName,
   useNetwork,
+  useSwitchNetwork,
 } from "wagmi";
+
+const supportedChainIds = [
+  5, // Goerli
+  1337, // Localhost
+];
 
 export default function Profile() {
   const { address, connector, isConnected } = useAccount();
   const { data: ensAvatar } = useEnsAvatar();
   const { data: ensName } = useEnsName();
-  const network = useNetwork();
   const { connect, connectors, error, isLoading, pendingConnector } =
     useConnect();
   const { disconnect } = useDisconnect();
+  const { chain } = useNetwork();
+  const {
+    chains,
+    error: switchNetworkError,
+    isLoading: isSwitchNetworkLoading,
+    pendingChainId,
+    switchNetwork,
+  } = useSwitchNetwork();
 
   if (isConnected) {
     return (
       <div className="flex flex-col gap-1">
         {ensAvatar && <img src={ensAvatar} alt="ENS Avatar" />}
         <div>{ensName ? `${ensName} (${address})` : address}</div>
+
         <div>连接到 {connector?.name}</div>
-        <div>当前网络 {network?.chain?.name}</div>
+        <div>当前网络 {chain?.name}</div>
         <Button onClick={() => disconnect()}>断开连接</Button>
+        {chain && !supportedChainIds.includes(chain?.id) && (
+          <div className="flex items-center gap-2">
+            <div>当前网络不支持，请切换到以下网络：</div>
+            {chains
+              .filter((chain) => supportedChainIds.includes(chain.id))
+              .map((chain) => (
+                <Button
+                  key={chain.id}
+                  disabled={isSwitchNetworkLoading}
+                  isLoading={
+                    isSwitchNetworkLoading && chain.id === pendingChainId
+                  }
+                  onClick={() => switchNetwork?.(supportedChainIds[0])}
+                  size="sm"
+                >
+                  {chain.name}
+                  {isSwitchNetworkLoading &&
+                    chain.id === pendingChainId &&
+                    " (切换中)"}
+                </Button>
+              ))}
+            {switchNetworkError && (
+              <Alert status="error">{switchNetworkError.message}</Alert>
+            )}
+          </div>
+        )}
       </div>
     );
   }
