3852e2ec60958c88975694f61d910f76e8b44bcd
Noah Lu <luzhenqian98@gmail.com>
2023-01-15 13:34:55 +0800

    feat: add SimpleBar

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,6 +1,14 @@
+import dynamic from "next/dynamic";
 import Head from "next/head";
 import Link from "next/link";
+import "simplebar-react/dist/simplebar.min.css";
 import examples from "../examples.json";
+
+// FIXME:
+// @ts-ignore
+const SimpleBar = dynamic(() => import("simplebar-react"), {
+  ssr: false,
+});
 
 export default function Home() {
   return (
@@ -21,45 +29,49 @@ export default function Home() {
           </Link>
         </div>
 
-        <div className="flex flex-col w-full h-full gap-4 p-4 overflow-auto rounded-lg shadow-sm md:gap-8 md:p-8 md:shadow-inner md:rounded-3xl md:backdrop-opacity-40 md:backdrop-invert">
-          <div className="w-full text-3xl font-bold md:text-white">
-            Web3 代码示例
-          </div>
-          <div className="grid grid-cols-1 gap-4 md:grid-cols-4">
-            {examples.map(({ name, description, url, technologyStack }) => (
-              <div
-                key={name}
-                className="flex flex-col gap-2 md:gap-4 p-2 transition-all duration-200 ease-in-out bg-white md:border-white rounded-md md:bg-inherit md:p-4 hover:border
+        {/* FIXME: */}
+        {/* @ts-ignore */}
+        <SimpleBar className="w-full h-full rounded-lg md:rounded-3xl">
+          <div className="flex flex-col w-full h-full gap-4 p-4 overflow-auto rounded-lg shadow-sm md:gap-8 md:p-8 md:shadow-inner md:rounded-3xl md:backdrop-opacity-40 md:backdrop-invert">
+            <div className="w-full text-3xl font-bold md:text-white">
+              Web3 代码示例
+            </div>
+            <div className="grid grid-cols-1 gap-4 md:grid-cols-4">
+              {examples.map(({ name, description, url, technologyStack }) => (
+                <div
+                  key={name}
+                  className="flex flex-col gap-2 md:gap-4 p-2 transition-all duration-200 ease-in-out bg-white md:border-white rounded-md md:bg-inherit md:p-4 hover:border
                 text-[#24292f] md:text-white border-gray-300 border md:border-0"
-              >
-                <div className="flex justify-between">
-                  <Link className="text-2xl" href={url}>
-                    {name}
-                  </Link>
+                >
+                  <div className="flex justify-between">
+                    <Link className="text-2xl" href={url}>
+                      {name}
+                    </Link>
 
-                  <Link
-                    href={"https://github.com/luzhenqian/web3-examples"}
-                    target="_blank"
-                  >
-                    <div className="flex items-center h-8 gap-2 px-2 border border-gray-300 rounded-md md:hidden">
-                      <StartIcon />
-                      <span>Start</span>
-                    </div>
-                  </Link>
+                    <Link
+                      href={"https://github.com/luzhenqian/web3-examples"}
+                      target="_blank"
+                    >
+                      <div className="flex items-center h-8 gap-2 px-2 border border-gray-300 rounded-md md:hidden">
+                        <StartIcon />
+                        <span>Start</span>
+                      </div>
+                    </Link>
+                  </div>
+                  <div className="text-sm text-[#57606a] md:text-inherit">
+                    {description}
+                  </div>
+                  <div className="text-sm font-bold">技术栈</div>
+                  <ul>
+                    {technologyStack.map((tech) => (
+                      <li key={tech}>{tech}</li>
+                    ))}
+                  </ul>
                 </div>
-                <div className="text-sm text-[#57606a] md:text-inherit">
-                  {description}
-                </div>
-                <div className="text-sm font-bold">技术栈</div>
-                <ul>
-                  {technologyStack.map((tech) => (
-                    <li key={tech}>{tech}</li>
-                  ))}
-                </ul>
-              </div>
-            ))}
+              ))}
+            </div>
           </div>
-        </div>
+        </SimpleBar>
       </main>
     </>
   );
