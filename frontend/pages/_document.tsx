35e2e9381ff87c0f9296ab910b643b5e3b15b99b
Noah Lu <luzhenqian98@gmail.com>
2023-01-16 23:19:03 +0800

    fix: mobile height

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -1,13 +1,13 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="zh-CN">
       <Head />
-      <body className='h-full h-[-webkit-fill-available]'>
+      <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
