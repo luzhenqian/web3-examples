da2b83c769a3555d9b5d083a929a0106c1de3dd2
Noah Lu <luzhenqian98@gmail.com>
2023-01-19 01:07:45 +0800

    test: nft

--- contract/noah-nft/test/noah-nft.js
+++ contract/noah-nft/test/noah-nft.js
@@ -2,37 +2,28 @@ const NoahNFT = artifacts.require("NoahNFT");
 
 contract('NoahNFT', (accounts) => {
   const [alice, bob, carol] = accounts;
+  const baseUrl = 'https://nft.webnext.cloud/'
   // 模拟 NFT 数据
   const [NFTNoOne, NFTNoTwo] = [{
-    uri: 'https://nft.webnext.cloud/1',
+    uri: '1',
   }, {
-    uri: 'https://nft.webnext.cloud/2',
+    uri: '2',
   }]
 
   it('mint', async () => {
-    const noahInstance = await NoahNFT.new('Noah', 'NOAH', { from: alice });
+    const noahInstance = await NoahNFT.new('Noah', 'NOAH', baseUrl, { from: alice });
     // alice 铸造 1 个 NFT 并发送给 bob
     await noahInstance.mint(bob, NFTNoOne.uri, { from: alice });
     // 检查 bob 的 NFT 数量是否为 1
     const bobNFTCount = await noahInstance.balanceOf(bob);
     assert.equal(bobNFTCount.valueOf(), 1, "bob 应该有 1 个 NFT");
     // 检查 NFT 的所有者是否为 bob
-    const tokenId = await noahInstance.currentTokenId();
+    const tokenId = await noahInstance.totalSupply();
     const bobNFT = await noahInstance.ownerOf(tokenId);
     assert.equal(bobNFT.valueOf(), bob, "bob 应该是 NFT 的所有者");
     // 检查 NFT 的 URI 是否正确
     const bobNFTURI = await noahInstance.tokenURI(tokenId, { from: bob });
-    assert.equal(bobNFTURI.valueOf(), NFTNoOne.uri, "NFT 的 URI 不正确");
-
-    try {
-      // bob 不是 NFT 的发行者，不能铸造 NFT
-      await noahInstance.mint(bob, NFTNoTwo.uri, { from: bob });
-    } catch (e) {
-      assert.include(e.message, 'caller is not the minter', 'bob 不是 NFT 的发行者，不能铸造 NFT')
-    }
-
-    // 添加 bob 为 NFT 的发行者
-    await noahInstance.approveMinter(bob, true, { from: alice });
+    assert.equal(bobNFTURI.valueOf(), baseUrl + NFTNoOne.uri, "NFT 的 URI 不正确");
     // bob 成为 NFT 的发行者，可以铸造 NFT
     await noahInstance.mint(bob, NFTNoTwo.uri, { from: bob });
     // 检查 bob 的 NFT 数量是否为 2
@@ -103,31 +94,5 @@ contract('NoahNFT', (accounts) => {
     // 检查 id 为 2 的 NFT 所有者是否为 bob
     const NFTNoTwoOwner = await noahInstance.ownerOf(2, { from: bob });
     assert.equal(NFTNoTwoOwner.valueOf(), bob, "id 为 2 的 NFT 所有者应该是 bob");
-  });
-
-  it("approvalMinter", async () => {
-    const noahInstance = await NoahNFT.new('Noah', 'NOAH', { from: alice });
-    // alice 授权 bob 成为铸造者
-    await noahInstance.approveMinter(bob, true, { from: alice });
-    // 检查 bob 是否被授权成为铸造者
-    const bobApproved = await noahInstance.isMinter(bob, { from: bob });
-    assert.equal(bobApproved.valueOf(), true, "bob 应该被授权成为铸造者");
-    // bob 铸造 1 个 NFT 并发送给 carol
-    await noahInstance.mint(carol, NFTNoOne.uri, { from: bob });
-    // 检查 carol 的 NFT 数量是否为 1
-    const carolNFTCount = await noahInstance.balanceOf(carol);
-    assert.equal(carolNFTCount.valueOf(), 1, "carol 应该有 1 个 NFT");
-    // 撤销 bob 的铸造者权限
-    await noahInstance.approveMinter(bob, false, { from: alice });
-    // 检查 bob 是否被授权成为铸造者
-    const bobApproved2 = await noahInstance.isMinter(bob, { from: bob });
-    assert.equal(bobApproved2.valueOf(), false, "bob 应该被撤销铸造者权限");
-    try {
-      // bob 再次尝试铸造 1 个 NFT 并发送给 carol
-      await noahInstance.mint(carol, NFTNoTwo.uri, { from: bob });
-      assert.fail("bob 应该没有铸造者权限");
-    } catch (e) {
-      assert.include(e.message, "caller is not the minter", "bob 应该没有铸造者权限");
-    }
   });
 });
